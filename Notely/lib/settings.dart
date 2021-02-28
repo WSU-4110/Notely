bea95be8f1f73305ea7fdc2afa06a04a100be31e
Sheev Patel <go5486@wayne.edu>
2021-02-27 22:36:39 -0500

    Update settings.dart

--- Notely/lib/settings.dart
+++ Notely/lib/settings.dart
@@ -5,35 +5,100 @@ import 'package:settings_ui/settings_ui.dart'; //Flutter Settings Package
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bool value = true; //Some bool variable we might need.. Dark Mode??
+    bool privateAccountValue = false;
+    String dropdownValue = '';
 
+    // "SettingsList" is from the package that I imported.
     return new SettingsList(
       sections: [
+        // Different setting section, titled 'account'
         SettingsSection(
-          title: 'Settings',
+          title: 'Account',
+          titlePadding: EdgeInsets.all(15),
           tiles: [
+            // Individual tiles
             SettingsTile(
               title: 'Language',
               subtitle: 'English',
               leading: Icon(Icons.language),
               onPressed: (BuildContext context) {},
             ),
-            SettingsTile.switchTile(
-              title: 'Use fingerprint',
-              leading: Icon(Icons.fingerprint),
-              switchValue: value,
-              onToggle: (bool value) {},
+            SettingsTile(
+              title: 'Change Email',
+              leading: Icon(Icons.email_outlined),
+              onTap: () {
+                //Code to change email will go here.
+              },
             ),
             SettingsTile(
-              title: 'Back',
-              leading: Icon(Icons.arrow_back_ios),
+              title: 'Change Major',
+              leading: Icon(Icons.history_edu_outlined),
               onTap: () {
-                Navigator.pop(context);
+                //Code to change major wll go here.
               },
             )
           ],
         ),
+
+        //Another section
+        SettingsSection(
+          title: 'Notifications',
+          titlePadding: EdgeInsets.all(15),
+          tiles: [
+            SettingsTile(
+                //Individual tiles
+                title: 'Change Alert',
+                leading: Icon(Icons.notifications),
+                //tried to code this to pull out a drop down, doesn't work
+                onTap: () {
+                  DropdownButton<String>(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.deepPurple),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <String>['One', 'Two', 'Free', 'Four']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  );
+                }),
+          ],
+        ),
+
+        SettingsSection(
+            title: 'Privacy',
+            titlePadding: EdgeInsets.all(15),
+            tiles: [
+              SettingsTile.switchTile(
+                title: 'Private Account',
+                leading: Icon(Icons.lock),
+                switchValue: privateAccountValue,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile(
+                title: 'Back',
+                leading: Icon(Icons.arrow_back_ios),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ])
       ],
     );
   }
+
+  void setState(Null Function() param0) {} // For future functionality
 }
