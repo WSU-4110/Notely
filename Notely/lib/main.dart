c9a9445288c80d80f49951c187d9473b0b925ff6
JulianGombosWSU <gl9647@wayne.edu>
2021-03-15 22:33:15 -0400

    Added a value for initialData for provider

--- Notely/lib/main.dart
+++ Notely/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
+      initialData: User(),
       child: MaterialApp(
         theme: ThemeData(
           accentColor: Colors.tealAccent.shade400,
