8b5a5c4aef6c8dcd57dd19fe2302506322cfd5f2
Christian Anthony <78061741+ex2358@users.noreply.github.com>
2021-04-08 00:07:32 -0400

    Update Postmaster.dart

--- Notely/lib/models/Postmaster.dart
+++ Notely/lib/models/Postmaster.dart
@@ -6,9 +6,8 @@ import 'package:provider/provider.dart';
 import 'dart:core';
 
 
-    // QuerySnapshot querySnapshot = await Firestore.instance.collection("collection").getDocuments();
-    // var list = querySnapshot.documents;
-
+  // QuerySnapshot querySnapshot = await Firestore.instance.collection("collection").getDocuments();
+  // var list = querySnapshot.documents;
 
 class Postmaster{
   //var postCollection;
@@ -16,8 +15,6 @@ class Postmaster{
   //File fileName; //This would be replaced with firebase url
   //DatabaseService database = new DatabaseService();
   
-  
-
   //QuerySnapshot querySnapshot
   
   Postmaster(){
@@ -26,9 +23,6 @@ class Postmaster{
     //print("Post text in 1st slot = " + postList[0].title);
     
     //postCollection = DatabaseService().getPostData();
-    
-
-    
   }
 
   void loadPosts(){
@@ -40,18 +34,12 @@ class Postmaster{
     //   postList.add(new Post(templist[0]))
     // }
     
+    //I had to hardcode these because file reading was giving me issues, and I need them for testing purposes
     var templist1 = ["Cats", "Big Cats", "Meow"];
     var templist2 = ["Reynolds", "Troll Toll", "Nightcrawlers"];
     var templist3 = ["Math", "Nerd stuff", "Whatever"];
-
-
-
-    //I had to hardcode these because file reading was giving me issues, and I need them for testing purposes
     postList.add(new Post("CSC 3200 Notes", "Bookworm97", "4/7/2021", false, templist1, ["https://3.bp.blogspot.com/-BDAGgWkK1-Y/VcPBhjZeR6I/AAAAAAAABiY/svh4aNkHPAo/s1600/IMG_8819_picmonkeyed.jpg"]));
     postList.add(new Post("ART 4100 Lecture", "Nightman_xx", "3/23/2021", false, templist2, ["https://kaylablogs.com/wp-content/uploads/2016/09/aphasia-notes.jpg"]));
     postList.add(new Post("MTH 2150 Complex Numbers", "pointDexter314", "3/9/2021", false, templist3, ["https://wallpapercave.com/wp/wp6810211.jpg"]));
-
-
-
   }
 }
