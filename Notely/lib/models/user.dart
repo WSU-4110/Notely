d9ccbb9c81d6a6f2a0beb9d859fb49ead182b2fa
Jacob Good <go1446@wayne.edu>
2021-04-15 19:39:24 -0400

    Created unit test for User class

--- Notely/lib/models/user.dart
+++ Notely/lib/models/user.dart
@@ -1,7 +1,28 @@
 class User {
-  final String uid;
-  final dynamic username;
-  final dynamic numberOfPosts;
+  String uid;
+  dynamic username;
+  dynamic numberOfPosts;
 
   User({this.uid, this.username, this.numberOfPosts});
+
+  String get userID{
+    return uid;
+  }
+  set userID(String userID){
+    this.uid = userID;
+  }
+
+  dynamic get uName{
+    return username;
+  }
+  set uName(dynamic uName){
+    this.username = uName;
+  }
+
+  dynamic get numPosts{
+    return numberOfPosts;
+  }
+  set numPosts(dynamic numPosts){
+    this.numberOfPosts = numPosts;
+  }
 }
